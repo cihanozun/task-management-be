17e98303332335d60153701d331e0bb4e4698eca
cihanozun <cihanozun@gmail.com>
2025-05-01 15:54:03 +0300

    WIP: adminService and wsc

---
+++ src/main/java/com/taskmanager/Task_Manager/controller/admin/AdminController.java
@@ -0,0 +1,4 @@
+package com.taskmanager.Task_Manager.controller.admin;
+
+public class AdminController {
+}
---
+++ src/main/java/com/taskmanager/Task_Manager/services/admin/AdminServiceImpl.java
@@ -0,0 +1,4 @@
+package com.taskmanager.Task_Manager.services.admin;
+
+public class AdminServiceImpl {
+}
